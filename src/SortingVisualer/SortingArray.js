8b884407cd0970acce61a9104e9f59067bb659fe
Taiye <taiyehige@gmail.com>
2020-12-01 23:40:58 +0000

    Organised and clesned

--- src/SortingVisualer/SortingArray.js
+++ src/SortingVisualer/SortingArray.js
@@ -1,48 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './SortingArray.css';
 
 function SortingArray(props){
 
-    //var divs = [];
-    const [divs = [], setdivs] = useState();
-    var divHeightArray = [];
+    const CreateDivs = () => {
+        const temm = [];
+        for (var i=0; i < props.DataArray.length; i++){
+            temm.push(<div className="div" style={{width: (100/props.DataArray.length - 0.4)+ "vw" , height: (0.6*props.DataArray[i]) + "vh"}}></div>);
 
-    for (var i=0; i < props.arraySize; i++){
-        var divHeight = Math.floor(Math.random()*100)
-        divHeightArray.push(divHeight);
-        divs.push(<div className="div" style={{width: (100/props.arraySize - 0.4)+ "vw" , height: (0.6*divHeight) + "vh"}}>
-        <p1></p1>
-        </div>) 
-        
-       
-    }
-
-    const BubbleSort = () =>{
-        var unsorted = true;
-        var x = props.arraySize - 1;
-        while (unsorted === true){
-            unsorted = false
-            for (var i=0; i < x; i++){
-                if (divHeightArray[i] > divHeightArray[i+1]){
-                    unsorted = true
-                    var temp = divHeightArray[i]
-                    var divstemp = setdivs[i]
-                    divHeightArray[i] = divHeightArray[i+1]
-                    setdivs[i] = setdivs[i+1]
-                    divHeightArray[i+1] = temp
-                    setdivs[i+1] = divstemp
-                    console.log(divHeightArray);
-                }
-                //console.log(unsorted);   
-            }
-            //unsorted = false
         }
+
+        return temm;
     }
 
+    console.log(props.DataArray);
     return(
         <div>
-            <button onClick={BubbleSort}>Bubble Sort</button>  
-            <div className="divbar">{divs}</div>
+            <div className="divbar">{CreateDivs()}</div>
         </div>
     )
 
--- src/SortingVisualer/SortingVis.js
+++ src/SortingVisualer/SortingVis.js
@@ -4,26 +4,67 @@ import SortingArray from './SortingArray'
 function SortingVis(){
 
     const sortSpeed = 1;
-    //var num;
+    const [DataArray, SetDataArray] = useState([]);
+    var arraySize = 5
+    
+    //var divs = [];
 
-    const [arraySize, setarraySize] = useState();
     
 
     const RandomArray = () =>{
-        setarraySize(2 + Math.floor(Math.random()*100));
-        console.log(arraySize);
+        arraySize = 2 + Math.floor(Math.random()*100);
+        CreateArray()
     }
     
     const arraychange = (num) =>{
         if (num > 2 && num < 100){
-            setarraySize(num);
-        }
-        else{
-            setarraySize(arraySize);
+            arraySize = num;
+            CreateArray()
         }
     }
 
+    const CreateArray = () =>{
+        var tempy = [];
+        for (var i=0; i < arraySize; i++){  
+            var divHeight = 1 + Math.floor(Math.random()*99);
+            tempy.push(divHeight);
+        }
+        SetDataArray(tempy)
+    }
+
+    const WaitTime = (delay) =>{
+        return new Promise(resolve => { 
+            setTimeout(() => { 
+                resolve(); 
+            }, delay); 
+        });
+    }
     
+
+    const BubbleSort = async() =>{
+        var unsorted = true;
+        var x = DataArray.length - 1;
+        while (unsorted === true){
+            unsorted = false;
+            for (var i=0; i < x; i++){
+                if (DataArray[i] > DataArray[i+1]){
+                    unsorted = true;
+                    var tempy2 = DataArray;
+                    var temp = tempy2[i];
+                    tempy2[i] = tempy2[i+1];
+                    tempy2[i+1] = temp;
+                    //console.log(DataArray);
+                    console.log(tempy2);
+                    
+                    await WaitTime(1000);
+                    //SetDataArray()
+                }
+                //console.log(unsorted);   
+            }        
+            //unsorted = false
+        }
+        SetDataArray(tempy2);
+    }
 
 
 
@@ -34,13 +75,9 @@ function SortingVis(){
             <button onClick={RandomArray}>Generate Random Array</button>
             <label>Array Size</label>
             <input type="text" onChange={e => {arraychange(e.target.value)}}></input>
+            <button onClick={BubbleSort}>Bubble Sort</button>  
 
-            
-
-            
-
-
-            <SortingArray arraySize = {arraySize}/>
+            <SortingArray arraySize={arraySize} DataArray={DataArray}/>
 
         </div>
     );
